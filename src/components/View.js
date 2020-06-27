a47ff4fe1be4b16a781daf7710b430a3e61f4a61
sarangparikh22 <sarangparikh22@gmail.com>
2020-06-28 02:29:59 +0530

    Update View.js

--- src/components/View.js
+++ src/components/View.js
@@ -43,7 +43,7 @@ export class View extends Component {
         alert('done');
 
     }
-    async componentDidUpdate(){
+    async componentDidMount(){
         let topicID = this.props.match.params.id;
         const { contract } = this.props
         let topicName = await contract.getTopicDetails({topicID})
