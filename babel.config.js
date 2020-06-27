d4eeea996a4985aa5bf74b456bd44f6a3fb80a90
Sarang Parikh <sarangparikh22@gmail.com>
2020-06-28 00:15:16 +0530

    solved build errors

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react']
-}
+  presets: ['@babel/preset-react', '@babel/preset-env'],
+  plugins: ['@babel/plugin-proposal-class-properties', 
+  '@babel/plugin-transform-runtime'],
+};
